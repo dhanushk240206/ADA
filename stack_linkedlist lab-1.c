7b30d0f8c06453433c64c8548afd193260128a16
dhanushk240206 <dhanushk.cs24@bmsce.ac.in>
2025-02-28 12:59:03 +0530

    Update stack_linkedlist lab-1.c

--- stack_linkedlist lab-1.c
+++ stack_linkedlist lab-1.c
@@ -62,14 +62,35 @@ void display(struct node* top){
 
 void main(){
       struct node * top = NULL;
-      top = push (top,10);
-      top = push (top,20);
-      top = push (top,30);
-      top = push (top,40);
-      top = push (top,50);
-      display (top);
-      top = pop (top);
-    display (top);
+       int value, choice;
+    while(1){
+        printf("\n\n*****Choice*****\n");
+        printf("1. push\n");
+        printf("2. pop\n");
+        printf("3. Display\n");
+        printf("4. exit\n");
+
+        printf("Enter the your choice: ");
+        scanf("%d",&choice);
+        switch(choice){
+            case 1:
+            printf("Enter the value to be insert: ");
+            scanf("%d",&value);
+           top= push(top,value);
+            break;
+            case 2: 
+            top=pop(top);
+            break;
+            case 3: display(top);
+            break;
+            case 4:
+            exit(1);
+            default:
+            printf("wrong selection!!!!");
+            break;
+        }
+    }
+    
        
 
 
